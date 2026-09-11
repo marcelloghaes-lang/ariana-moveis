f25aa71b61e3a997eaac9d5e5c1eea4a6a757e7e
marcelloghaes-lang <marcelloghaes@gmail.com>
2026-09-11 15:13:53 -0300

    feat: registra rotas da Etapa 2 SIGE

--- functions/routes/televendas/index.js
+++ functions/routes/televendas/index.js
@@ -3,6 +3,7 @@ import createTelevendasRouter from './televendasRoutes.js';
 import createErpManagementRoutes from '../erp/erpManagementRoutes.js';
 import createErpSigeMigrationRoutes from '../erp/erpSigeMigrationRoutes.js';
 import createErpSigeMasterDataRoutes from '../erp/erpSigeMasterDataRoutes.js';
+import createErpSigeHistoryRoutes from '../erp/erpSigeHistoryRoutes.js';
 
 export default function createTelevendasRoutes(context={}){
   const router=express.Router();
@@ -10,5 +11,6 @@ export default function createTelevendasRoutes(context={}){
   router.use(createErpManagementRoutes(context));
   router.use(createErpSigeMigrationRoutes(context));
   router.use(createErpSigeMasterDataRoutes(context));
+  router.use(createErpSigeHistoryRoutes(context));
   return router;
 }
