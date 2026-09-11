f3e1063abd226deaafddc5e8066978290b7c7ae9
marcelloghaes-lang <marcelloghaes@gmail.com>
2026-09-11 15:47:19 -0300

    feat: conecta clientes nativos ao ERP

--- functions/routes/televendas/index.js
+++ functions/routes/televendas/index.js
@@ -3,6 +3,7 @@ import createTelevendasRouter from './televendasRoutes.js';
 import createErpManagementRoutes from '../erp/erpManagementRoutes.js';
 import createErpSigeMigrationRoutes from '../erp/erpSigeMigrationRoutes.js';
 import createErpSigeMasterDataRoutes from '../erp/erpSigeMasterDataRoutes.js';
+import createErpPeopleRoutes from '../erp/erpPeopleRoutes.js';
 
 export default function createTelevendasRoutes(context={}){
   const router=express.Router();
@@ -10,5 +11,6 @@ export default function createTelevendasRoutes(context={}){
   router.use(createErpManagementRoutes(context));
   router.use(createErpSigeMigrationRoutes(context));
   router.use(createErpSigeMasterDataRoutes(context));
+  router.use(createErpPeopleRoutes(context));
   return router;
 }
