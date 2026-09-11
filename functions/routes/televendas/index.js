09322cad37af9831444534c19df19872b6a7daa9
marcelloghaes-lang <marcelloghaes@gmail.com>
2026-09-11 09:37:58 -0300

    feat: conecta rotas de gestao do Ariana ERP

--- functions/routes/televendas/index.js
+++ functions/routes/televendas/index.js
@@ -1,2 +1,10 @@
+import express from 'express';
 import createTelevendasRouter from './televendasRoutes.js';
-export default createTelevendasRouter;
+import createErpManagementRoutes from '../erp/erpManagementRoutes.js';
+
+export default function createTelevendasRoutes(context={}){
+  const router=express.Router();
+  router.use(createTelevendasRouter(context));
+  router.use(createErpManagementRoutes(context));
+  return router;
+}
