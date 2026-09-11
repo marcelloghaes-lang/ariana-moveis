cdba00f2e17f9c969f6d42b22fb5e7f0f60dc9fd
marcelloghaes-lang <marcelloghaes@gmail.com>
2026-09-10 23:07:39 -0300

    security: register storefront payment intent guard

--- functions/routes/legacyRoutes.js
+++ functions/routes/legacyRoutes.js
@@ -1,4 +1,5 @@
 import registerLegacyRuntimeRoutes from './legacyRuntimeRoutes.js';
+import registerStorefrontPaymentIntentGuard from './storefrontPaymentIntentGuard.js';
 import registerStorefrontPaymentSecurity from './storefrontPaymentSecurity.js';
 import registerStorefrontOrderSecurity from './storefrontOrderSecurity.js';
 import registerStorefrontCheckoutIntegrity from './storefrontCheckoutIntegrity.js';
@@ -167,6 +168,7 @@ function buildRuntimeContext(context = {}) {
 export default function registerLegacyRoutes(app, context = {}) {
   const runtimeContext = buildRuntimeContext(context);
   runtimeContext.axios = createMercadoPagoIdempotentAxios(runtimeContext.axios, runtimeContext.crypto);
+  registerStorefrontPaymentIntentGuard(app, runtimeContext);
   const paymentSecurity = registerStorefrontPaymentSecurity(app, runtimeContext);
   Object.assign(runtimeContext, paymentSecurity || {});
   registerStorefrontOrderSecurity(app, runtimeContext);
