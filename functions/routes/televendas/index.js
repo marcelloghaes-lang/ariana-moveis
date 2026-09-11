e2078e7cb2cf32f0a00aa27308ef56eef2b86f19
marcelloghaes-lang <marcelloghaes@gmail.com>
2026-09-11 13:03:23 -0300

    feat: conecta importação dos cadastros mestres SIGE

--- functions/routes/televendas/index.js
+++ functions/routes/televendas/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import createTelevendasRouter from './televendasRoutes.js';
 import createErpManagementRoutes from '../erp/erpManagementRoutes.js';
 import createErpSigeMigrationRoutes from '../erp/erpSigeMigrationRoutes.js';
+import createErpSigeMasterDataRoutes from '../erp/erpSigeMasterDataRoutes.js';
 
 export default function createTelevendasRoutes(context={}){
   const router=express.Router();
   router.use(createTelevendasRouter(context));
   router.use(createErpManagementRoutes(context));
   router.use(createErpSigeMigrationRoutes(context));
+  router.use(createErpSigeMasterDataRoutes(context));
   return router;
 }
