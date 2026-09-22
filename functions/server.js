0db1ca5aac2263be9887fce4cfa70f4f956cbb4a
marcelloghaes-lang <marcelloghaes@gmail.com>
2026-09-22 13:18:03 -0300

    ERP: corrigir impressão e automatizar lembrete do vencimento
    Corrige páginas vazias na impressão dos vencimentos do dia e adiciona lembrete WhatsApp idempotente para clientes com parcela vencendo hoje.

--- functions/server.js
+++ functions/server.js
@@ -36,6 +36,7 @@ import createTelevendasRoutes from './routes/televendas/index.js';
 import registerCieloRoutes from './routes/cieloRoutes.js';
 import { releaseExpiredStockReservations } from './services/stockReservationService.js';
 import { startAdminWhatsappReminderWorker } from './services/adminWhatsappAlertService.js';
+import { startErpDailyDueWhatsappWorker } from './services/erp/erpDailyDueWhatsappService.js';
 import initModels from './models/index.js';
 
 
@@ -2925,6 +2926,15 @@ app.post('/api/coupons/validate', async (req, res, next) => {
 registerCoraRoutes(app, { adminRequired, authRequired, mongoose, Order, Product });
 registerCrediarioAnalysisRoutes(app, { adminRequired, authRequired, mongoose, Order, Product, waSendTextMessage });
 startAdminWhatsappReminderWorker({ Order, mongoose, waSendTextMessage });
+startErpDailyDueWhatsappWorker({
+  Order,
+  Setting,
+  IntegrationAuditLog,
+  mongoose,
+  waSendTextMessage,
+  toJSON,
+  redact
+});
 registerCrediarioConversationRoutes(app, { mongoose, adminRequired });
 registerAdminUserRoutes(app, { User, AdminAuditLog, AdminSession, AdminLoginEvent, adminRequired, bcrypt, mongoose, isSuperAdminEmail });
 
